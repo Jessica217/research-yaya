a8fbd3b5372d908757aac074c3c8436af96e9958
yaya <2439070462@qq.com>
2023-09-26 17:33:46 +0800

    修改day2-Clock.py文件，新添属性和类内方法

--- codes/day2-Clock.py
+++ codes/day2-Clock.py
@@ -1,9 +1,10 @@
 import time
+import datetime
+import pytz
 
+class Clock(object): # 基类Clock
 
-class Clock(object):
-
-    def __init__(self, hour: int = 0, minute: int = 0, second: int = 0):  # init方法用于初始化类的实例
+    def __init__(self, hour: int = 0, minute: int = 0, second: int = 0 ):  # init方法用于初始化类的实例
         self.hour = hour  # 实例属性
         self.minute = minute
         self.second = second
@@ -28,14 +29,44 @@ class Clock(object):
         # print("{}:".format(self.hour), "{}:".format(self.minute), "{}".format(self.second)) 这样写太笨重
         print("%.2d:%.2d:%.2d"%(self.hour, self.minute, self.second)) # %.2d是将一个整数格式化为两位数的字符串，.2是精确到小数点后两位
 
-if __name__ == "__main__": # 此方法用于确定当前脚本是被直接运行，还是被作为模块导入到其他脚本中
-    # 如果在当前脚本中，会被直接运行 但如果作为模块导入到其他脚本中，则不会被执行，此方法很适合用于代码的重用和模块化
-    Clock = Clock(23, 59, 58)
+class AdvanceClock(Clock): # 创建Clock的子类AdvanceClock,新增加两个属性，timezone和city_name
 
+    def __init__(self, hour, minute, second, timezone:dict = {}, city_name:str = "", timezone_num:str = ""): # 在为子类添加属性时，必须要写父类的属性
+        super().__init__(hour, minute, second) # 调用父类的构造方法
+        self.timezone = timezone
+        self.city_name = city_name
+        self.timezone_num = timezone_num
+
+    def get_time(self): # 新增类内方法
+        self.timezone_num = datetime.datetime.now() # 当前时实时间
+        #self.city_name = input("输入时区的城市名:")
+
+        if self.city_name == "Beijing":
+            print("输出时区{}".format(self.city_name),"时间为{}".format(self.timezone_num.strftime("%Y-%m-%d %H:%M:%S")))
+
+        elif self.city_name == "NewYork":
+            newyork = pytz.timezone('US/Eastern')
+            current_time_ny = self.timezone_num.astimezone(newyork)
+            print("输入时区{}".format(self.city_name),"的时间为{}".format(current_time_ny.strftime("%Y-%m-%d %H:%M:%S")))
+
+        elif self.city_name == "London":
+            london = pytz.timezone('Europe/London')
+            current_time_london = self.timezone_num.astimezone(london)
+            print("输入时区{}".format(self.city_name), "的时间为{}".format(current_time_london.strftime("%Y-%m-%d %H:%M:%S")))
+
+if __name__ == "__main__":  # 此方法用于确定当前脚本是被直接运行，还是被作为模块导入到其他脚本中
+    # 如果在当前脚本中，会被直接运行 但如果作为模块导入到其他脚本中，则不会被执行，此方法很适合用于代码的重用和模块化
+    # Clock = Clock(23, 59, 58)
     # 使用while true进入无限循环 一直打印输出
     while True:
-        Clock.run()
+        # 创建子类AdvanceClock的实例
+        clock_time = AdvanceClock(hour=23,minute=58,second=59,
+                                  timezone={"Beijing":"{timezone_num}", "NewYork":"{timezone_num}", "London":"{timezone_num}"},
+                                  city_name = input("请输入时区的城市名:"))
+
+        clock_time.run()
         time.sleep(1)
-        Clock.show()
+        clock_time.get_time()
+
 
 # 示例如上，原先分数80!
