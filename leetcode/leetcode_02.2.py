b94032194fcc4c8470e17262c78e43a97639f51a
yaya <2439070462@qq.com>
2023-10-31 20:05:35 +0800

    修改transform和train文件

--- leetcode/leetcode_02.2.py
+++ leetcode/leetcode_02.2.py
@@ -10,12 +10,21 @@ class SingleLinkList():
 
 
 if __name__ == "__main__":
+
+
+
     singlelinklist = SingleLinkList() # 创建空链表
     node1 = Node(1) # 创建结点1
     node2 = Node(2) # 创建结点2
+    node3 = Node(3) # 创建结点3
 
     singlelinklist.head = node1 # 将node1添加到链表中
     node1.next = node2
+    node2.next = node3
 
-    print(singlelinklist.head.item)
+    print(type(singlelinklist.head.item))
     print(singlelinklist.head.next.item)
+
+
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
